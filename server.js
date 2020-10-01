74d15a2547878eac1f95540dd7ffce2ccb7f76e9
Марат <ihsmen@iMac-Marat.local>
2020-10-01 05:50:59 +0300

    Express server setup with initial routes

---
+++ server.js
@@ -0,0 +1,18 @@
+// Фреймворк для серверной части сайта на базе Node.js
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API...' }))
+
+
+// Определяем пути
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+
+// Настраиваем порт, в одном случае если удут запускать уже готовый сайт и в другом локальный для разработки
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT , () => console.log(`Server started on port ${PORT}`));
