289ef8e23065b61a76b65a041415b47822b51182
Marat <m.ihsanov2009@yandex.ru>
2020-10-16 17:17:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,13 +10,19 @@ connectDB();
 // Запускаем middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API...' }))
-
 
 // Определяем пути
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 
 // Настраиваем порт, в одном случае если удут запускать уже готовый сайт и в другом локальный для разработки
